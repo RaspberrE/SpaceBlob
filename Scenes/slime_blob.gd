eac96ab0e984a870343e5d47070b9c052ba3d8f1
Raf <Rafael.solorzanonunez@gihs.sa.edu.au>
2025-08-03 18:21:37 +0930

    Added collectible slime blobs!
    (no ui yet)

--- Scenes/slime_blob.gd
+++ Scenes/slime_blob.gd
@@ -1,11 +1,34 @@
 extends Node2D
 
+signal add_blob
+@onready var area: Area2D = $SlimeBlobANIM/Area2D
+
+var player_in_area = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$Label.visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("Interact"):
+		Globalvars.blob_amount += 1
+		print(Globalvars.blob_amount)
+		queue_free()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.name == "PLAYER":
+		$Label.visible = true
+		player_in_area = true
+		
+	print(body.name)
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.name == "PLAYER":
+		$Label.visible = false
+		player_in_area = false
+
+
+func _on_add_blob() -> void:
+	pass # Replace with function body.
---
+++ Scripts/globalvars.gd
@@ -0,0 +1,11 @@
+extends Node
+
+var blob_amount = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
